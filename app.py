36b1d2bd22685ff984756e396744c1c10272dff3
MicaelOliveira-dev <micaeldev@hotmail.com>
2022-08-28 19:44:56 -0300

    Projeto Finalizado

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from routes.user import user
 
-app = FastAPI()
+app = FastAPI(
+    title="REST API COM FASTAPI",
+    description="Simples APi com mongoDB"
+)
 
 app.include_router(user)
--- routes/user.py
+++ routes/user.py
@@ -1,17 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 from models.user import User
 from config.db import conn
 from schemas.user import userEntity, usersEntity
 from bson import ObjectId
+from starlette.status import HTTP_204_NO_CONTENT
 
 
 user = APIRouter()
 
-@user.get('/users')
+@user.get('/users', response_model=list[User])
 def find_all_user():
     return usersEntity(conn.local.user.find())
 
-@user.post('/users')
+@user.post('/users', response_model=User)
 def create_user(user: User):
     new_user = dict(user)
     del new_user["id"]
@@ -19,17 +20,17 @@ def create_user(user: User):
     user = conn.local.user.find_one({"_id": id})
     return userEntity(user)
 
-@user.get('/users/{id}')
+@user.get('/users/{id}', response_model=User)
 def find_user(id: str):
     return userEntity(conn.local.user.find_one({"_id": ObjectId(id)}))
     
-@user.put('/users/{id}')
-def update_user():
-    return "Hello World"
+@user.put('/users/{id}', response_model=User)
+def update_user(id: str, user: User):
+    conn.local.user.find_one_and_update(
+        {"_id": ObjectId(id)}, {"$set": dict(user)})
+    return userEntity(conn.local.user.find_one({"_id": ObjectId(id)}))
 
-@user.delete('/users/{id}')
-def delete_user():
-    return "Hello World"
-
-
-    #43:12
+@user.delete('/users/{id}', status_code=status.HTTP_204_NO_CONTENT )
+def delete_user(id: str):
+    userEntity(conn.local.user.find_one_and_delete({"_id": ObjectId(id)}))
+    return Response(status_code=HTTP_204_NO_CONTENT)
